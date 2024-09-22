a0a7c9b552f9dc7587051742ca96089c044cf280
Tasniem Seiam <tasniemseiam@gmail.com>
2024-09-22 12:00:08 +0300

    comment local api

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,7 @@ let client;
 let clientPromise;
 let db;
 const uri = "mongodb+srv://tasniem:yGvkbgmfq8n8DIr7@cluster0.tika6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// const uri = "mongodb://localhost:27017";
+
 
 if (!uri) {
   throw new Error("Please add your MongoDB URI to .env.local");
